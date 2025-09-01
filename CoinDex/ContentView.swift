a3c686662c672a7e5757c84092b27ff43a7f7952
Pranav <pranav1160ly@gmail.com>
2025-09-01 09:11:46 +0530

    initial commit

--- CoinDex/ContentView.swift
+++ CoinDex/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.pink.ignoresSafeArea()
+            VStack{
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
         }
-        .padding()
     }
 }
 
