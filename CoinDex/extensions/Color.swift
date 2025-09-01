ba0e20f12ceae80459b0a69d0ca5384f3387bd65
Pranav <pranav1160ly@gmail.com>
2025-09-01 10:40:53 +0530

    [Feature ] added Color extension to support custom themes

---
+++ CoinDex/extensions/Color.swift
@@ -0,0 +1,22 @@
+//
+//  Color.swift
+//  CoinDex
+//
+//  Created by Pranav on 01/09/25.
+//
+
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme{
+    let appRed = Color("AppRedColor")
+    let appGreen = Color("AppGreenColor")
+    let accent = Color("AppAccentColor")
+    let backGround = Color("AddBackgroundColor")
+    let secondaryText = Color("AppSecondaryTextColor")
+}
