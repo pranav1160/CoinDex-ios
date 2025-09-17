606cf832bc5eadbee3df5bbee2d4c6f064d1710a
Pranav <pranav1160ly@gmail.com>
2025-09-17 21:55:59 +0530

    [Feature] detail view completed except chart

--- CoinDex/core/detail/view/DetailView.swift
+++ CoinDex/core/detail/view/DetailView.swift
@@ -5,56 +5,159 @@
 //  Created by Pranav on 12/09/25.
 //
 
-
 import SwiftUI
 
+
 struct DetailView: View {
-    @StateObject var vm: CoinDetailViewModel
-    let coin: Coin
+    
+    @StateObject private var vm: CoinDetailViewModel
+    @State private var showFullDescription: Bool = false
+    private let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
+    private let spacing: CGFloat = 30
     
     init(coin: Coin) {
-        self.coin = coin
         _vm = StateObject(wrappedValue: CoinDetailViewModel(coin: coin))
     }
     
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading, spacing: 16) {
+            VStack {
+               
                 
-                // Coin Image
-                AsyncImage(url: URL(string: coin.image)) { img in
-                    img.resizable()
-                        .scaledToFit()
-                        .frame(width: 100, height: 100)
-                } placeholder: {
-                    ProgressView()
+                VStack(spacing: 20) {
+                    overviewTitle
+                    Divider()
+                    descriptionSection
+                    overviewGrid
+                    additionalTitle
+                    Divider()
+                    additionalGrid
+                    websiteSection
                 }
-                .frame(maxWidth: .infinity, alignment: .center)
-                
-                // Coin Name + Symbol
-                VStack(alignment: .leading, spacing: 8) {
-                    Text(coin.name)
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                    
-                    Text(coin.symbol.uppercased())
-                        .font(.headline)
-                        .foregroundColor(.secondary)
-                }
-                
-                Divider()
-                
-                
+                .padding()
             }
-            .padding()
         }
-        .navigationTitle(coin.name)
-        .navigationBarTitleDisplayMode(.inline)
+        .background(
+            Color.theme.backGround
+                .ignoresSafeArea()
+        )
+        .navigationTitle(vm.coin.name)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                navigationBarTrailingItems
+            }
+        }
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(coin: dev.coin)
+        NavigationView {
+            DetailView(coin: dev.coin)
+        }
     }
+}
+
+extension DetailView {
+    
+    private var navigationBarTrailingItems: some View {
+        HStack {
+            Text(vm.coin.symbol.uppercased())
+                .font(.headline)
+                .foregroundColor(Color.theme.secondaryText)
+            ImageView(coin: vm.coin)
+                .frame(width: 25, height: 25)
+        }
+    }
+    
+    private var overviewTitle: some View {
+        Text("Overview")
+            .font(.title)
+            .bold()
+            .foregroundColor(Color.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var additionalTitle: some View {
+        Text("Additional Details")
+            .font(.title)
+            .bold()
+            .foregroundColor(Color.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var descriptionSection: some View {
+        ZStack {
+            if let coinDescription = vm.coinDescription,
+               !coinDescription.isEmpty {
+                VStack(alignment: .leading) {
+                    Text(coinDescription)
+                        .lineLimit(showFullDescription ? nil : 3)
+                        .font(.callout)
+                        .foregroundColor(Color.theme.secondaryText)
+                    
+                    Button(action: {
+                        withAnimation(.easeInOut) {
+                            showFullDescription.toggle()
+                        }
+                    }, label: {
+                        Text(showFullDescription ? "Less" : "Read more..")
+                            .font(.caption)
+                            .fontWeight(.bold)
+                            .padding(.vertical, 4)
+                    })
+                    .accentColor(.blue)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+    }
+    
+    private var overviewGrid: some View {
+        LazyVGrid(
+            columns: columns,
+            alignment: .leading,
+            spacing: spacing,
+            pinnedViews: [],
+            content: {
+                ForEach(vm.overviewStatistics) { stat in
+                    StatisticView(stat: stat)
+                }
+            })
+    }
+    
+    private var additionalGrid: some View {
+        LazyVGrid(
+            columns: columns,
+            alignment: .leading,
+            spacing: spacing,
+            pinnedViews: [],
+            content: {
+                ForEach(vm.additionalStatistics) { stat in
+                    StatisticView(stat: stat)
+                }
+            })
+    }
+    
+    private var websiteSection: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            if let websiteString = vm.websiteURL,
+               let url = URL(string: websiteString) {
+                Link("Website", destination: url)
+            }
+            
+            if let redditString = vm.redditURL,
+               let url = URL(string: redditString) {
+                Link("Reddit", destination: url)
+            }
+            
+        }
+        .accentColor(.blue)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .font(.headline)
+    }
+    
 }
--- CoinDex/core/detail/viewModel/CoinDetailViewModel.swift
+++ CoinDex/core/detail/viewModel/CoinDetailViewModel.swift
@@ -9,21 +9,91 @@ import Foundation
 import Combine
 
 class CoinDetailViewModel:ObservableObject{
-    @Published var coinDetails:CoinDetail?
-    private let detailService : CoinDetailDataService
-    private var cancellables =  Set<AnyCancellable>()
-    init(coin:Coin){
-        self.detailService = CoinDetailDataService(coin: coin)
-        addSubscribers()
+    
+    @Published var overviewStatistics: [Statistic] = []
+    @Published var additionalStatistics: [Statistic] = []
+    @Published var coinDescription: String? = nil
+    @Published var websiteURL: String? = nil
+    @Published var redditURL: String? = nil
+    
+    @Published var coin: Coin
+    private let coinDetailService: CoinDetailDataService
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(coin: Coin) {
+        self.coin = coin
+        self.coinDetailService = CoinDetailDataService(coin: coin)
+        self.addSubscribers()
     }
     
-    private func addSubscribers(){
-        detailService.$coinDetails
-            .sink { returedDetails in
-                print("GOT THE DATA BRO!!!!")
-                print(returedDetails)
+    private func addSubscribers() {
+        
+        coinDetailService.$coinDetails
+            .combineLatest($coin)
+            .map(mapDataToStatistics)
+            .sink { [weak self] (returnedArrays) in
+                self?.overviewStatistics = returnedArrays.overview
+                self?.additionalStatistics = returnedArrays.additional
             }
             .store(in: &cancellables)
+        
+    }
+    
+    
+    private func mapDataToStatistics(coinDetailModel: CoinDetail?, coinModel: Coin) -> (overview: [Statistic], additional: [Statistic]) {
+        let overviewArray = createOverviewArray(coinModel: coinModel)
+        let additionalArray = createAdditionalArray(coinDetailModel: coinDetailModel, coinModel: coinModel)
+        return (overviewArray, additionalArray)
+    }
+    
+    private func createOverviewArray(coinModel: Coin) -> [Statistic] {
+        let price = coinModel.currentPrice.asCurrencyWith2to6Decimals()
+        let pricePercentChange = coinModel.priceChangePercentage24H
+        let priceStat = Statistic(title: "Current Price", value: price, percentageChange: pricePercentChange)
+        
+        let marketCap = "$" + (coinModel.marketCap?.formattedWithAbbreviations() ?? "")
+        let marketCapPercentChange = coinModel.marketCapChangePercentage24H
+        let marketCapStat = Statistic(title: "Market Capitalization", value: marketCap, percentageChange: marketCapPercentChange)
+        
+        let rank = "\(coinModel.rank)"
+        let rankStat = Statistic(title: "Rank", value: rank)
+        
+        let volume = "$" + (coinModel.totalVolume?.formattedWithAbbreviations() ?? "")
+        let volumeStat = Statistic(title: "Volume", value: volume)
+        
+        let overviewArray: [Statistic] = [
+            priceStat, marketCapStat, rankStat, volumeStat
+        ]
+        return overviewArray
+    }
+    
+    private func createAdditionalArray(coinDetailModel: CoinDetail?, coinModel: Coin) -> [Statistic] {
+        
+        let high = coinModel.high24H?.asCurrencyWith2to6Decimals() ?? "n/a"
+        let highStat = Statistic(title: "24h High", value: high)
+        
+        let low = coinModel.low24H?.asCurrencyWith2to6Decimals() ?? "n/a"
+        let lowStat = Statistic(title: "24h Low", value: low)
+        
+        let priceChange = coinModel.priceChange24H?.asCurrencyWith2to6Decimals() ?? "n/a"
+        let pricePercentChange = coinModel.priceChangePercentage24H
+        let priceChangeStat = Statistic(title: "24h Price Change", value: priceChange, percentageChange: pricePercentChange)
+        
+        let marketCapChange = "$" + (coinModel.marketCapChange24H?.formattedWithAbbreviations() ?? "")
+        let marketCapPercentChange = coinModel.marketCapChangePercentage24H
+        let marketCapChangeStat = Statistic(title: "24h Market Cap Change", value: marketCapChange, percentageChange: marketCapPercentChange)
+        
+        let blockTime = coinDetailModel?.blockTimeInMinutes ?? 0
+        let blockTimeString = blockTime == 0 ? "n/a" : "\(blockTime)"
+        let blockStat = Statistic(title: "Block Time", value: blockTimeString)
+        
+        let hashing = coinDetailModel?.hashingAlgorithm ?? "n/a"
+        let hashingStat = Statistic(title: "Hashing Algorithm", value: hashing)
+        
+        let additionalArray: [Statistic] = [
+            highStat, lowStat, priceChangeStat, marketCapChangeStat, blockStat, hashingStat
+        ]
+        return additionalArray
     }
     
 }
